7f304013781be7224647c1dd20ee5b203e236828
Robert Neijmeijer <Robert.Neijmeijer@hva.nl>
2020-12-31 20:30:40 +0100

    Route page

--- app/src/main/java/com/example/capstonereisplanner/ui/MainActivity.kt
+++ app/src/main/java/com/example/capstonereisplanner/ui/MainActivity.kt
@@ -13,18 +13,11 @@ import com.example.capstonereisplanner.viewmodel.StationViewModel
 import com.example.capstonereisplanner.viewmodel.TripViewModel
 
 class MainActivity : AppCompatActivity() {
-    private val viewModel: TripViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-        //Log.d("TRIP", viewModel.getTrip("WUPPH","HAMM").trips.toString())
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
